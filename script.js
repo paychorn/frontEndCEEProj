ab807e47a16eb7225b12469c0f868fcbdf896a88
Vivatsathorn Thitasirivit <neil4884@gmail.com>
2023-04-26 09:38:43 +0700

    Cleanup something

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+const coll = document.getElementsByClassName("collapsible");
+let i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    const content = this.nextElementSibling;
     if (content.style.display === "none") {
       content.style.display = "block";
     } else {
