870c92d93b58f75964b6a6afa15d1a146c79dc1e
Vivatsathorn Thitasirivit <neil4884@gmail.com>
2023-05-03 03:34:24 +0700

    Finished

--- js/script-mcv.js
+++ js/script-mcv.js
@@ -1,8 +1,8 @@
 const appName = "MCV Companion";
 const useTest = false;
 const useSecure = false;
-const backendAddress = 'localhost:3000'
-// const backendAddress = `mcv.vt.in.th:3000`;
+// const backendAddress = 'localhost:3000'
+const backendAddress = `54.235.87.205:3000`;
 const MyPage = {
   LOGIN: 0,
   MAIN: 1
